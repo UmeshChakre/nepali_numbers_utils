31805745894f8a629b6ccf8d8d5a31308dbfa3fb
Umesh <umesh.chakradhar@8squarei.com>
2024-04-15 20:11:20 +0545

    Liscence added and test file updated

--- test/numbers_to_nepali_word_converter_test.dart
+++ test/numbers_to_nepali_word_converter_test.dart
@@ -1,16 +1,5 @@
-import 'package:numbers_to_nepali_word_converter/numbers_to_nepali_word_converter.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
+  group('A group of tests', () {});
 }
